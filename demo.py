38301969b2e5503eae2aca53d0ca5b7764f5c2c2
Jason Zigelbaum <jasonzigelbaum@gmail.com>
2012-04-02 19:01:55 -0500

    ignoring and removing .pyc files

--- demo.py
+++ demo.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
   # Get file
   fin = getFile('trainData/train.tsv')
   data = createDataObject(fin)
-  
+  # Print some sample data to make sure it works.
   print data['WGAAD-148']['GI_10047091-S']
