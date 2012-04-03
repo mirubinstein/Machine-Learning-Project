f26fb90f4c0df73331fe64fbe729eefd2f2de7ab
Mark Rubinstein <mirubinstein@gmail.com>
2012-04-03 18:48:01 -0500

    Fixed dictionary builder.

--- fileUtil.py
+++ fileUtil.py
@@ -24,7 +24,6 @@ def getFile(path=None):
   
 def createDataObject(fin):
   """Creates a data object for this data. The format is a dictionary of id's which have a dictionary of genes within them. Call like this : data['Target ID']['Gene'] """
-  
   # Get the target ids
   targetIds = fin.readline().rstrip().split('\t')
   targetIds.pop(0)
@@ -36,17 +35,17 @@ def createDataObject(fin):
   lines = fin.readlines()
   geneTypes = []
   
-  # Collect All Genes
+  # Collect All Genes Types
   for line in lines:
-    geneTypes.append(line.rstrip().split('\t').pop(0))
-  
-  # Zip each set of genes into a dictionary
-  for line in lines:
-    gene = line.rstrip().split('\t')
-    gene.pop(0)
-    genes.append(dict( zip( geneTypes, gene ) ))
-    
-  # Zip genes into a dictionary by target id
-  data = dict( zip( targetIds, genes ) )
+    geneArray = line.rstrip().split('\t');
+    geneTypes.append(geneArray.pop(0))
+    genes.append(geneArray)
+
+  data = {}
+  for person in range(len(targetIds)):
+    indivData = []
+    for gene in range(len(geneTypes)):
+      indivData.append(genes[gene][person])
+    data[targetIds[person]] = dict(zip(geneTypes, indivData))
 
   return data
